553e3e24a38a51c153c0fcf99193f806b62e86b3
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2025-11-14 13:30:23 +0530

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
+
+Route::get('/', [PortfolioController::class, 'home'])->name('home');
+Route::get('/index', [PortfolioController::class, 'home'])->name('home');
+Route::get('/about', [PortfolioController::class, 'about'])->name('about');
+Route::get('/work', [PortfolioController::class, 'work'])->name('work');
+Route::get('/skills', [PortfolioController::class, 'skills'])->name('skills');
+Route::get('/contact', [PortfolioController::class, 'contact'])->name('contact');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
