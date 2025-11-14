d2c10b34f610b4b71ab1105d2ef10d80c02ea893
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2025-11-14 14:54:02 +0530

    Add files via upload

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PortfolioController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function work()
+    {
+        return view('work');
+    }
+
+    public function skills()
+    {
+        return view('skills');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+}
